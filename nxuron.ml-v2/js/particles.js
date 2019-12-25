bd2131416e0db22bae210a5370f7bfed1ad89506
sohamb117 <sohamb117@gmail.com>
2019-12-24 21:28:35 -0800

    Nope

--- nxuron.ml-v2/js/particles.js
+++ nxuron.ml-v2/js/particles.js
@@ -1530,3 +1530,10 @@ window.particlesJS.load = function(tag_id, path_config_json, callback) {
 };
 particlesJS("particles-js", { "particles": { "number": { "value": 159, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 0.5, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 1, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 1, "direction": "none", "random": true, "straight": false, "out_mode": "bounce", "bounce": false, "attract": { "enable": true, "rotateX": 1200, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "grab" }, "onclick": { "enable": true, "mode": "bubble" }, "resize": true }, "modes": { "grab": { "distance": 100, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 100, "size": 5, "duration": 0.5, "opacity": 0.5, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true });
 console.log("Hi")
+
+window.onload = function() {
+    Particles.init({
+        selector: '.background',
+        color: '#294'
+    });
+};
